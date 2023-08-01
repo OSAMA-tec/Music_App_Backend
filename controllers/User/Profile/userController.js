f927a41aac7c11c76c3b40f191b35e732404ff39
Osama-tec <hashmiosama555@gmail.com>
2023-08-01 20:56:58 +0500

    make it better update profile fix

--- controllers/User/Profile/userController.js
+++ controllers/User/Profile/userController.js
@@ -24,24 +24,26 @@ const getUser = async (req, res) => {
   };
 
 
-
-
   const updateUser = async (req, res) => {
     const { email, name, phoneNumber } = req.body;
     const id = req.user.id;
   
-    if (!email  && !name && !phoneNumber) {
+    if (!email && !name && !phoneNumber) {
       return res.status(400).json({ msg: 'Enter at least one field to update: email, name, or phoneNumber' });
     }
   
     try {
-        const otp = Math.floor(100000 + Math.random() * 900000);
+      const otp = Math.floor(100000 + Math.random() * 900000);
       const user = await User.findById(id);
       if (!user) {
         return res.status(404).json({ msg: 'User not found' });
       }
   
       if (email) {
+        const emailExists = await User.findOne({ 'local.email': email });
+        if (emailExists && emailExists.id !== id) {
+          return res.status(400).json({ msg: 'Email already exists' });
+        }
         user.local.email = email;
       }
   
@@ -50,31 +52,34 @@ const getUser = async (req, res) => {
       }
   
       if (phoneNumber) {
-        if(!email){
-            user.local.tempEmail = user.local.email;
+        const phoneNumberExists = await User.findOne({ 'local.phoneNumber': phoneNumber });
+        if (phoneNumberExists && phoneNumberExists.id !== id) {
+          return res.status(400).json({ msg: 'Phone number already exists' });
         }
-        else{
-            user.local.tempEmail = email;
+        if (!email) {
+          user.local.tempEmail = user.local.email;
+        } else {
+          user.local.tempEmail = email;
         }
         user.local.tempNumber = phoneNumber;
         user.local.verified = false;
-        user.local.otp= otp;
+        user.local.otp = otp;
         twilioClient.messages.create({
-            body: `Your OTP for MUSIC APP is: ${otp}`,
-            from: process.env.TWILIO_PHONE_NUMBER,
-            to: phoneNumber
-          })
-            .then(() => { console.log('OTP sent via SMS') })
-            .catch((error) => { console.error(error); })
-            
+          body: `Your OTP for MUSIC APP is: ${otp}`,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: phoneNumber,
+        })
+          .then(() => { console.log('OTP sent via SMS') })
+          .catch((error) => { console.error(error); });
+  
       }
   
       await user.save();
-
-      if(!phoneNumber){
-          return res.status(200).json({ msg: 'User updated successfully!' });
-        }else{
-          return res.status(200).json({ msg: 'User updated successfully! and Kindly enter Otp' });
+  
+      if (!phoneNumber) {
+        return res.status(200).json({ msg: 'User updated successfully!' });
+      } else {
+        return res.status(200).json({ msg: 'User updated successfully! and Kindly enter Otp' });
       }
   
     } catch (err) {
@@ -82,8 +87,6 @@ const getUser = async (req, res) => {
       res.status(500).send('Server error');
     }
   };
-
-
 const deleteUser=async (req,res)=>{
     try{
         const id = req.user.id;
