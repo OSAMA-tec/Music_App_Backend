60e8d5c8b703b4b139058999ee8ee6d9a55f4c99
Osama-tec <hashmiosama555@gmail.com>
2023-08-01 21:40:41 +0500

    make it better merch create added

---
+++ controllers/Artist/createMerch.js
@@ -0,0 +1,75 @@
+const Merchandise = require('../../../models/Merchandise');
+const Artist = require('../../../models/Artist');
+const { bucket, admin } = require('../../config/firbaseConfig');
+const multer = require('multer');
+const createMerch = async (req, res) => {
+    const { name, description, price } = req.body;
+    const image = req.file;
+    let artistId = req.user.id;
+    // Check for missing input data
+    if (!name  || !price || !image) {
+      return res.status(400).json({
+        success: false,
+        message: 'Missing required fields: name, artistId, price, and image',
+      });
+    }
+  
+    try {
+      const artist = await Artist.findById(artistId);
+      if (!artist) {
+        return res.status(400).json({
+          success: false,
+          message: 'No Artist Found',
+        });
+      }
+  
+      const fileName = `merchandise/${Date.now()}-${image.originalname}`;
+      const fileUpload = bucket.file(fileName);
+  
+      const blobStream = fileUpload.createWriteStream({
+        metadata: {
+          contentType: image.mimetype,
+        },
+      });
+  
+      blobStream.on('error', (error) => {
+        console.error('Error uploading file:', error);
+        return res.status(500).json({
+          success: false,
+          message: 'Error uploading file',
+        });
+      });
+  
+      blobStream.on('finish', async () => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(
+          fileName
+        )}?alt=media`;
+  
+        const merch = new Merchandise({
+          name,
+          artist: artistId,
+          description,
+          price,
+          imageUrl,
+        });
+  
+        await merch.save();
+  
+        return res.status(200).json({
+          success: true,
+          message: 'Merchandise Created Successfully',
+          merch,
+        });
+      });
+  
+      blobStream.end(image.buffer);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        success: false,
+        message: 'An error occurred while creating the merchandise',
+      });
+    }
+  };
+  
+  module.exports = { createMerch };
---
+++ models/Order.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  clothType: {
+    type: String,
+    default: 'shirt',
+  },
+  size: {
+    type: String,
+    enum: ['small', 'medium', 'large'],
+    required: true,
+  },
+  orderDetails: {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    merch: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Merch',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  status: {
+    type: String,
+    enum: ['not_verified', 'verified', 'delivered'],
+    default: 'not_verified',
+  },
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
