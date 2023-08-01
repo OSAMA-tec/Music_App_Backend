7d393756c5759891a9cceafffe5ea925abefc78c
Osama-tec <hashmiosama555@gmail.com>
2023-08-01 14:53:17 +0500

    make it better fixes register updates something

--- controllers/User/Registration/signup.js
+++ controllers/User/Registration/signup.js
@@ -116,8 +116,17 @@ exports.registerUser = async (req, res) => {
   let email = '';
 
   try {
+    let userE = await User.findOne({ 'local.tempEmail': tempEmail });
     let userEmail = await User.deleteMany({ 'local.tempEmail': tempEmail });
+
+    let userP = await User.findOne({ 'local.tempPhone': tempEmail });
     let userPh = await User.deleteMany({ 'local.tempPhone': tempEmail });
+    if(userE){
+      await Artist.deleteMany({userId:userE._id})
+    }
+    if(userP){
+      await Artist.deleteMany({userId:userP._id})
+    }
     let user = await User.findOne({ 'local.email': tempEmail });
     const verified = false;
     // Set the timezone you want to use
