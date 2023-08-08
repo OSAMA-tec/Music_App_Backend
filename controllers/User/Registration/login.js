62be362c06d2518b570a1180f34949949c7fd9c2
Osama-tec <hashmiosama555@gmail.com>
2023-08-08 11:09:53 +0500

    email and phone both otp

--- controllers/User/Registration/login.js
+++ controllers/User/Registration/login.js
@@ -2,15 +2,133 @@ const User = require('../../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const twilio = require('twilio');
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+require('dotenv').config();
 
 const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+async function sendOtpViaEmail(email, otp) {
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+  const apiKey = defaultClient.authentications['api-key'];
+  apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
 
-exports.loginUser = async (req, res) => {
-  const { phoneNumber } = req.body;
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+  sendSmtpEmail.to = [{ email: email }];
+  sendSmtpEmail.subject = 'GHM MUSIC APP OTP';
+  sendSmtpEmail.htmlContent = `<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+        background-color: #ffffff;
+        padding: 20px;
+        box-sizing: border-box;
+      }
+      .header {
+        text-align: center;
+        padding-bottom: 20px;
+        border-bottom: 1px solid #e5e5e5;
+      }
+      .header h1 {
+        font-size: 24px;
+        color: #333333;
+        margin: 0;
+      }
+      .content {
+        padding: 20px 0;
+        text-align: center;
+      }
+      .content p {
+        font-size: 16px;
+        color: #666666;
+        line-height: 1.5;
+        margin: 0;
+      }
+      .otp {
+        font-size: 24px;
+        font-weight: bold;
+        color: #00466a;
+        margin: 20px 0;
+      }
+      .footer {
+        text-align: center;
+        padding-top: 20px;
+        border-top: 1px solid #e5e5e5;
+        font-size: 12px;
+        color: #999999;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <h1>MUSIC APP</h1>
+      </div>
+      <div class="content">
+        <p>Thank you for using MUSIC APP. Please use the following One-Time Password (OTP) to complete your registration process. This OTP is valid for 5 minutes.</p>
+        <div class="otp">${otp}</div>
+        <p>If you did not request this OTP, please ignore this email.</p>
+      </div>
+      <div class="footer">
+        &copy; MUSIC APP. All rights reserved.
+      </div>
+    </div>
+  </body>
+  </html>`;
+  sendSmtpEmail.sender = { email: process.env.EMAIL_FROM };
 
   try {
-    let user = await User.findOne({ 'local.phoneNumber': phoneNumber });
-    if (!user || !user.local.verified || user.local.phoneNumber === '') {
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    console.log('OTP sent via Email');
+  } catch (error) {
+    console.error('Failed to send OTP via Email:', error);
+    throw error;
+  }
+}
+exports.loginUser = async (req, res) => {
+  const { phoneNumber, email, verify } = req.body;
+  if (!verify) {
+    return res.status(400).json({
+      success: false,
+      message: 'Verify not passed',
+    });
+  }
+  if (!phoneNumber && !email) {
+    return res.status(400).json({
+      success: false,
+      message: 'Value not passed',
+    });
+  }
+
+  try {
+    let user;
+    if (phoneNumber) {
+      user = await User.findOne({ 'local.phoneNumber': phoneNumber });
+    }
+    else if (email) {
+      user = await User.findOne({ 'local.email': email });
+    }
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid credentials',
+      });
+    }
+
+    if (!user.local.verified || user.local.phoneNumber === '') {
       return res.status(400).json({
         success: false,
         message: 'Invalid credentials',
@@ -18,22 +136,39 @@ exports.loginUser = async (req, res) => {
     }
 
     const otp = Math.floor(100000 + Math.random() * 900000);
-    const otpHash = await bcrypt.hash(otp.toString(), 10);
 
-    user.local.otp = otpHash;
+    user.local.otp = otp;
     user.local.otpCreatedAt = Date.now();
     await user.save();
 
-    // Send OTP via SMS
-    twilioClient.messages.create({
-      body: `Your OTP for MUSIC APP is: ${otp}`,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: phoneNumber
-    })
-      .then(() => { console.log('OTP sent via SMS') })
-      .catch((error) => { console.error(error); })
-
-    res.json({ message: 'OTP sent to your phone number. Please verify.' });
+    if (verify === 'email') {
+      await sendOtpViaEmail(email, otp);
+      res.json({
+        message: 'Registered successfully. Please verify your email.',
+      });
+    } else if (verify === 'phone') {
+      twilioClient.messages
+        .create({
+          body: `Your OTP for MUSIC APP is: ${otp}`,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: phoneNumber,
+        })
+        .then(() => {
+          console.log('OTP sent via SMS');
+          res.json({
+            message:
+              ' Please verify your phone number.',
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({
+            msg: 'Failed to send OTP via SMS. Please try again later.',
+          });
+        });
+    } else {
+      res.status(400).json({ msg: 'Invalid verify value' });
+    }
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
--- controllers/User/Registration/signup.js
+++ controllers/User/Registration/signup.js
@@ -9,8 +9,6 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 const twilio = require('twilio');
-const moment = require('moment-timezone');
-
 require('dotenv').config();
 
 SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = process.env.SENDINBLUE_API_KEY;
@@ -107,8 +105,103 @@ exports.googleAuthCallback = function (req, res, next) {
     });
   })(req, res, next);
 };
+
+
+// Function to send OTP via Email using Sendinblue
+async function sendOtpViaEmail(email, otp) {
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+  const apiKey = defaultClient.authentications['api-key'];
+  apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
+
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+  sendSmtpEmail.to = [{ email: email }];
+  sendSmtpEmail.subject = 'GHM MUSIC APP OTP';
+  sendSmtpEmail.htmlContent = `<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+        background-color: #ffffff;
+        padding: 20px;
+        box-sizing: border-box;
+      }
+      .header {
+        text-align: center;
+        padding-bottom: 20px;
+        border-bottom: 1px solid #e5e5e5;
+      }
+      .header h1 {
+        font-size: 24px;
+        color: #333333;
+        margin: 0;
+      }
+      .content {
+        padding: 20px 0;
+        text-align: center;
+      }
+      .content p {
+        font-size: 16px;
+        color: #666666;
+        line-height: 1.5;
+        margin: 0;
+      }
+      .otp {
+        font-size: 24px;
+        font-weight: bold;
+        color: #00466a;
+        margin: 20px 0;
+      }
+      .footer {
+        text-align: center;
+        padding-top: 20px;
+        border-top: 1px solid #e5e5e5;
+        font-size: 12px;
+        color: #999999;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <h1>MUSIC APP</h1>
+      </div>
+      <div class="content">
+        <p>Thank you for using MUSIC APP. Please use the following One-Time Password (OTP) to complete your registration process. This OTP is valid for 5 minutes.</p>
+        <div class="otp">${otp}</div>
+        <p>If you did not request this OTP, please ignore this email.</p>
+      </div>
+      <div class="footer">
+        &copy; MUSIC APP. All rights reserved.
+      </div>
+    </div>
+  </body>
+  </html>`;
+  sendSmtpEmail.sender = { email: process.env.EMAIL_FROM };
+
+  try {
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    console.log('OTP sent via Email');
+  } catch (error) {
+    console.error('Failed to send OTP via Email:', error);
+    throw error;
+  }
+}
+
 exports.registerUser = async (req, res) => {
-  const { password, role, phoneNumber, name } = req.body;
+  const { password, role, phoneNumber, name,verify } = req.body;
   const tempEmail = req.body.email;
   let email = '';
   if(!phoneNumber || !name || !tempEmail){
@@ -175,27 +268,35 @@ exports.registerUser = async (req, res) => {
     if (roleSpecificDoc) {
       await roleSpecificDoc.save();
     }
-
-    // Send OTP via SMS
-    twilioClient.messages
-      .create({
-        body: `Your OTP for MUSIC APP is: ${otp}`,
-        from: process.env.TWILIO_PHONE_NUMBER,
-        to: phoneNumber,
-      })
-      .then(() => {
-        console.log('OTP sent via SMS');
-        res.json({
-          message:
-            'Registered successfully. Please verify your phone number.',
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).json({
-          msg: 'Failed to send OTP via SMS. Please try again later.',
-        });
+    
+ if (verify === 'email') {
+      await sendOtpViaEmail(tempEmail, otp);
+      res.json({
+        message: 'Registered successfully. Please verify your email.',
       });
+    } else if (verify === 'phone') {
+      twilioClient.messages
+        .create({
+          body: `Your OTP for MUSIC APP is: ${otp}`,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: phoneNumber,
+        })
+        .then(() => {
+          console.log('OTP sent via SMS');
+          res.json({
+            message:
+              'Registered successfully. Please verify your phone number.',
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({
+            msg: 'Failed to send OTP via SMS. Please try again later.',
+          });
+        });
+    } else {
+      res.status(400).json({ msg: 'Invalid verify value' });
+    }
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
@@ -203,14 +304,23 @@ exports.registerUser = async (req, res) => {
 };
 
 exports.verifyUser = async (req, res) => {
-  const { phoneNumber, otp } = req.body;
+  const { phoneNumber, email, otp } = req.body;
 
   try {
-    var user = await User.findOne({ 'local.tempNumber': phoneNumber });
-    if (!user) {
-       user = await User.findOne({ 'local.phoneNumber': phoneNumber });
+    let user;
+    if (phoneNumber) {
+      user = await User.findOne({ 'local.tempNumber': phoneNumber });
+      if (!user) {
+        user = await User.findOne({ 'local.phoneNumber': phoneNumber });
+      }
+    } else if (email) {
+      user = await User.findOne({ 'local.tempEmail': email });
+      if (!user) {
+        user = await User.findOne({ 'local.email': email });
+      }
     }
-    else{
+
+    if (!user) {
       return res.status(400).json({ msg: 'User does not exist' });
     }
     if (otp===user.local.otp) {
